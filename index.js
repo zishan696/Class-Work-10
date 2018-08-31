da2d612dc9e170185a4b1d38e494ee6687f56aa9
Ahshan Zishan <dantex696@outlook.com>
2018-08-31 01:46:50 +0000

    New Update

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
 app.get('/index', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+io.emit('some event', { for: 'everyone' });
+
 io.on('connection', function(socket){
   socket.broadcast.emit('hi');
+});
+
+io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-       io.emit('chat message', msg);
-    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
 });
 
-io.emit('some event', { for: 'everyone' });
-
-
-http.listen(process.env.PORT, function(){
-  console.log('listening on *:process.env.PORT');
-});
-
+http.listen(process.env.PORT, process.env.IP, function(){
+    console.log('Server running');
+  });
